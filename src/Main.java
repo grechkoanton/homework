f5825fd4ed9659b39dde11d71b5cb5c73ae1b42b
HP <grechkoantonkrd@gmail.com>
2025-03-31 15:42:10 +0300

    The first task

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        //переменной dog значение 8.0;переменной cat значение 3.6;
+        // переменной paper значение 763789.Вывести значения в консоль
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+}
