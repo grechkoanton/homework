c7e79f907160bf1ca29b23b25bb8df8422bd9fbe
HP <grechkoantonkrd@gmail.com>
2025-04-01 01:00:39 +0300

    The fourth task

--- src/Main.java
+++ src/Main.java
@@ -28,5 +28,17 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        //Инициализируйте (присвойте значение) переменную friend значением 19
+        //Увеличьте значение переменной на 2, после чего поделите значение на 7
+        //После каждой операции выводите значение переменной в консоль.
+        // В итоге у вас должно быть выведено 3 значения одной переменной.
+        //Для решения этой задачи используйте тип переменной var
+        System.out.println();
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
     }
 }
