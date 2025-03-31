a3397d091289d6b7907d4a3606ef03daca9c9420
HP <grechkoantonkrd@gmail.com>
2025-03-31 16:18:08 +0300

    The second task

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,15 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        //Увеличьте значение каждой перечисленной в прошлой задаче переменной на 4.
+        //Для решения этой задачи используйте тип переменной var.
+        //Выведите в консоль новые значения переменных.
+        System.out.println();
+        dog = 8.0 + 4;
+        cat = 3.6 + 4;
+        paper = 763789 + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
     }
 }
